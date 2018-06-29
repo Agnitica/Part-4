c1a8333b66deab120e144134be8d6dea906e7e4e
Agnitica <37901353+Agnitica@users.noreply.github.com>
2018-06-29 14:01:04 +0530

    Add files via upload

---
+++ destruction.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruction : MonoBehaviour {
+
+	// Use this for initialization
+
+	private float d;
+	public Scroll sc;
+	public mountainscroll ms;
+	public mountainscroll msn;
+	public GameObject player;
+	void Start () {
+		d = player.transform.position.x - transform.position.x;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		transform.position=new Vector3(player.transform.position.x-d,transform.position.y,transform.position.z);
+
+		GameObject a=GameObject.FindGameObjectWithTag ("backg");
+		if (a != null) {
+
+
+			if (a.transform.position.x < transform.position.x) {
+
+				sc.back.Remove (a);
+				Destroy (a);
+			}
+		}
+
+		GameObject b=GameObject.FindGameObjectWithTag ("mountainfar");
+		if (b != null) {
+			
+			if (b.transform.position.x < transform.position.x) {
+				
+				ms.mts.Remove (b);
+				Destroy (b);
+			}
+		}
+	
+		GameObject c=GameObject.FindGameObjectWithTag ("mountainnear");
+		if (c != null) {
+
+
+			if (c.transform.position.x < transform.position.x) {
+				
+				msn.mts.Remove (c);
+				Destroy (c);
+			}
+		}
+	
+	}
+}
---
+++ mountainscroll.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mountainscroll : MonoBehaviour {
+
+
+	public GameObject[] m;
+	public List<GameObject> mts;
+	public float hspeed=-2f;
+	public CameraController cam;
+	private Vector3 distance;
+	// Use this for initialization
+	void Awake ()
+	{
+		mts = new List<GameObject> ();
+		GameObject a = Instantiate (m[Random.Range (0, m.Length)]);
+		GameObject b = Instantiate (m[Random.Range (0, m.Length)]);
+		GameObject c = Instantiate (m[Random.Range (0, m.Length)]);
+		a.transform.position = new Vector3 (transform.position.x + a.transform.GetComponent<SpriteRenderer> ().size.x, transform.position.y, transform.position.z);
+		b.transform.position = new Vector3 (a.transform.position.x + b.transform.GetComponent<SpriteRenderer> ().size.x, transform.position.y, transform.position.z);
+		c.transform.position = new Vector3 (b.transform.position.x + c.transform.GetComponent<SpriteRenderer> ().size.x, transform.position.y, transform.position.z);
+
+
+		mts.Add (a);
+		mts.Add (b);
+		mts.Add (c);
+	
+		transform.position =c.transform.position;
+
+	}
+
+
+	void Start () {
+
+		distance =   transform.position-cam.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x - cam.transform.position.x < distance.x) { 
+			GameObject sc = (GameObject) Instantiate (m [Random.Range (0, m.Length)]);
+
+			sc.transform.position = new Vector3 (transform.position.x + sc.transform.GetComponent<SpriteRenderer> ().size.x, transform.position.y, transform.position.z);
+			mts.Add (sc);
+			Debug.Log (mts.Count);
+			transform.position=sc.transform.position;
+
+		}
+
+		for (int i = 0; i < mts.Count; i++) {
+			mts[i].transform.position += Vector3.right * hspeed * Time.deltaTime;
+
+		}
+
+		transform.position += Vector3.right * hspeed * Time.deltaTime;
+	}
+}
